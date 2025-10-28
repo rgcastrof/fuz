673de686e7d387d126ec54dbd785fc9b98aab9db
rgcastrof <rogeriogirao1@proton.me>
2025-10-28 18:39:54 -0300

    refactor main

--- src/main.c
+++ src/main.c
@@ -6,14 +6,17 @@ int
 main(int argc, char *argv[])
 {
 	Term term;
-	Vector *entry = create_vector();
-	char *line = NULL;
-	size_t buffer_size = 0;
-	ssize_t readed_chars;
+	size_t buffer_cap = INITIAL_ENTRYBUF_SIZE;
+	char *entry_buffer = malloc(buffer_cap);
+	Vector offsets;
+	vector_init(&offsets);
 
-	load_entry(entry, line, buffer_size, readed_chars);
-	load_curses(&term, entry);
-	cleanup(&term, entry);
+	load_entry(&offsets, &entry_buffer, &buffer_cap);
+	setup(&term);
+	size_t selected_offset = run(&term, &offsets, entry_buffer);
+	printf("%s\n", entry_buffer + selected_offset);
+	free(entry_buffer);
+	free(offsets.items);
 
 	return 0;
 }
