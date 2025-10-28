831b6bc038de2ada2a71c33c2c8b648250ac376f
rgcastrof <rogeriogirao1@proton.me>
2025-10-28 18:27:02 -0300

    add logic to dynamically realloc the entries buffer and push the offsets of entries into a vector

--- src/data_handler/handler.c
+++ src/data_handler/handler.c
@@ -3,18 +3,46 @@
 
 #include "handler.h"
 
-void
-load_entry(Vector *entry, char *line, size_t buffer_size, char *entrybuf)
+static int
+buffer_realloc(size_t pos, size_t len, char **entry_buffer, size_t *buffer_cap)
 {
+	size_t new_cap = *buffer_cap ? (*buffer_cap) * 2 : INITIAL_ENTRYBUF_SIZE;
+
+	while (new_cap < pos + len + 1)
+		new_cap *= 2;
+
+	char *tmp = realloc(*entry_buffer, new_cap);
+
+	if (!tmp) {
+		perror("Error: failed to realloc buffer");
+		return 0;
+	}
+
+	*entry_buffer = tmp;
+	*buffer_cap = new_cap;
+
+	return 1;
+}
+
+void
+load_entry(
+		Vector *offsets,
+		char **entry_buffer,
+		size_t *buffer_cap
+	)
+{
+	char *line = NULL;
+	size_t getline_buffer_size = 0;
 	size_t pos = 0;
-	while (getline(&line, &buffer_size, stdin) != -1) {
+	while (getline(&line, &getline_buffer_size, stdin) != -1) {
 		line[strcspn(line, "\n")] = 0;
 		size_t l = strlen(line);
 
-		memcpy(entrybuf + pos, line, l + 1);
+		if (pos + l + 1 > *buffer_cap)
+			buffer_realloc(pos, l, entry_buffer, buffer_cap);
 
-		char *ptr = entrybuf + pos;
-		vector_push_back(entry, &ptr);
+		memcpy(*entry_buffer + pos, line, l + 1);
+		vector_push_back(offsets, pos);
 
 		pos += l + 1;
 	}
--- src/data_handler/handler.h
+++ src/data_handler/handler.h
@@ -1,8 +1,10 @@
 #ifndef HANDLER_H
 #define HANDLER_H
 
+#define INITIAL_ENTRYBUF_SIZE (1024 * 1024)
+
 #include "../vector/vector.h"
 
-void load_entry(Vector *entry, char *line, size_t buffer_size, char *entrybuf);
+void load_entry(Vector *entry, char **entry_buffer, size_t *buffer_cap);
 
 #endif
